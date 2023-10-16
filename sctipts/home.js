7308666bac6ac5324cd5f39eaaca8c89615d1f21
mahdi <mahdimamashli1383@gmail.com>
2023-10-16 23:15:31 +0330

    add js in slider content

--- sctipts/home.js
+++ sctipts/home.js
@@ -18,3 +18,28 @@ function selectLangHand() {
 		showLang = true;
 	}
 }
+let sliderNewsImages = document.querySelectorAll(".sliderNews__images ");
+
+let wrapper = document.querySelector(".wrapper");
+sliderNewsImages.forEach(images => {
+	images.addEventListener("mouseover", () => {
+		let child = images.children;
+		let text = child[0];
+		text.style.cssText =
+			"transition: all 1s;opacity: .8;bottom: 60px;z-index:20;";
+		let img = child[1];
+		img.style.cssText =
+			"border-bottom-right-radius: 40px;border-top-left-radius: 40px;transition: all 1s;filter: blur(2px);";
+	});
+	images.addEventListener("mouseout", () => {
+		let child = images.children;
+		let text = child[0];
+		text.style.cssText =
+			"transition: all 1s;opacity: 0;bottom: 0px;z-index:20;";
+		let img = child[1];
+		img.style.cssText =
+			"border-bottom-right-radius: 5px;border-top-left-radius: 5px;transition: all 1s;filter: blur(0px);";
+	});
+	
+});
+// ;
