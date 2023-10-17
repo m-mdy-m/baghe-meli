fdb074d85d67f1241c5a9bf29948b5475dde38d3
mahdi <mahdimamashli1383@gmail.com>
2023-10-17 23:59:46 +0330

    change slider and add auto change imges

--- sctipts/home.js
+++ sctipts/home.js
@@ -40,6 +40,26 @@ sliderNewsImages.forEach(images => {
 		img.style.cssText =
 			"border-bottom-right-radius: 5px;border-top-left-radius: 5px;transition: all 1s;filter: blur(0px);";
 	});
-	
 });
 // ;
+// slider
+
+let Slider = document.querySelector(".introduction__slider");
+let SliderImages = document.querySelectorAll(".introduction__slider-photo");
+let sliderItem = document.querySelectorAll(".introduction__slider-item");
+
+let index = 0;
+function autoChange() {
+	setTimeout(autoChange, 2000);
+	var pic;
+	for (pic = 0; pic < sliderItem.length; pic++) {
+		sliderItem[pic].style.opacity = "0";
+		sliderItem[pic].style.transition = "all 2s";
+	}
+	index++;
+	if (index > sliderItem.length) {
+		index = 1;
+	}
+	sliderItem[index - 1].style.opacity = "1";
+}
+autoChange();
