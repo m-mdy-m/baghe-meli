d3e610db27ddb742de8093cf62a7179d8699d9cf
mahdi <mahdimamashli1383@gmail.com>
2023-10-30 18:39:54 +0330

    add new list and change content navigation

--- sctipts/home.js
+++ sctipts/home.js
@@ -3,6 +3,39 @@ let Slider = document.querySelector(".introduction__slider");
 let SliderImages = document.querySelectorAll(".introduction__slider-photo");
 let sliderItem = document.querySelectorAll(".introduction__slider-item");
 
+let showList = document.querySelector(".navItems-link");
+let listNav = document.querySelector(".navItems__lists");
+
+let showLists = document.querySelector(".nav-link");
+let listsNav = document.querySelector(".nav__lists");
+
+let shows = true;
+
+showLists.addEventListener("click", () => {
+	if (shows) {
+		listsNav.style.cssText =
+			"height: auto;width:120px;transform: scale(1);opacity: 1;";
+		shows = false;
+	} else {
+		listsNav.style.cssText =
+			"height: 0;width:120px;transform: scale(0);opacity: 0;";
+		shows = true;
+	}
+});
+
+let show = true;
+showList.addEventListener("click", () => {
+	if (show) {
+		listNav.style.cssText =
+			"height: auto;width:120px;transform: scale(1);opacity: 1;";
+		show = false;
+	} else {
+		listNav.style.cssText =
+			"height: 0;width:120px;transform: scale(0);opacity: 0;";
+		show = true;
+	}
+});
+
 let index = 0;
 function autoChange() {
 	setTimeout(autoChange, 5000);
