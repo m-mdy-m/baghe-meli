cef3ab510d5e6c1422c91d12ab00a42f455bd20e
mahdi <mahdimamashli1383@gmail.com>
2023-11-17 16:18:25 +0330

    add bgImage

--- src/srcripts.js
+++ src/srcripts.js
@@ -1,3 +1,5 @@
+let body = document.body
+console.log(body.offsetWidth);
 const listDescription = document.querySelectorAll('.list-description');
 const listItems = document.querySelectorAll('.list-items');
 
