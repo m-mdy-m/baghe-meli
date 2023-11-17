1fd09c17ae71be5223219a54eb411a847f1f7c80
mahdi <mahdimamashli1383@gmail.com>
2023-11-17 14:19:36 +0330

    Styling and display of header and navigation content is complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./layout/*.html'],
+  content: ["./layout/*.html","./src/*.js"],
   theme: {
     extend: {
       fontFamily: {
-				Vazir: "Vazir",
-				Vazir_Light: "Vazir-Light",
-				Vazir_Bold: "Vazir-Bold",
-				Vazir_Black: "Vazir-Black",
-				Vazir_Medium: "Vazir-Medium",
-				Aktiv: "Aktiv",
-			},
-  },
-  screens: {
-    small: {'max':"479px"},
-    mobile: "480px ",
-    laptop: "991px",
-    desktop: "1366px",
-    large: {'min':'1920px'},
-  },
-  container: {
-    center: true,
-    padding: {
-      DEFAULT: "1rem",
-      small: ".5rem",
-      mobile: ".75rem",
-      laptop: "1rem",
-      desktop: "2rem",
-      large: "3rem",
+        vazir: "vazir",
+        vazir_Light: "vazir-Light",
+        vazir_Bold: "vazir-Bold",
+        vazir_Black: "vazir-Black",
+        vazir_Medium: "vazir-Medium",
+      },
+    },
+    screens: {
+      small: { max: "479px" },
+      mobile: "480px ",
+      laptop: "991px",
+      desktop: "1366px",
+      large: { min: "1920px" },
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        small: ".5rem",
+        mobile: ".75rem",
+        laptop: "1rem",
+        desktop: "2rem",
+        large: "3rem",
+      },
     },
   },
-  },
   plugins: [],
-}
-
+};
