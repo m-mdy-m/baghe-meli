c8803cb16c00aebbbb617f40dc27e4ba2b86b4f6
mahdi <mahdimamashli1383@gmail.com>
2023-11-17 04:09:37 +0330

    config container

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,34 @@
 module.exports = {
   content: ['./layout/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+				Vazir: "Vazir",
+				Vazir_Light: "Vazir-Light",
+				Vazir_Bold: "Vazir-Bold",
+				Vazir_Black: "Vazir-Black",
+				Vazir_Medium: "Vazir-Medium",
+				Aktiv: "Aktiv",
+			},
+  },
+  screens: {
+    small: {'max':"479px"},
+    mobile: "480px ",
+    laptop: "991px",
+    desktop: "1366px",
+    large: {'min':'1920px'},
+  },
+  container: {
+    center: true,
+    padding: {
+      DEFAULT: "1rem",
+      small: ".5rem",
+      mobile: ".75rem",
+      laptop: "1rem",
+      desktop: "2rem",
+      large: "3rem",
+    },
+  },
   },
   plugins: [],
 }
